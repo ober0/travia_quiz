165acea2c948984dedd4d05073b7c5a94ee962be
Ruslan <onishruslan@yandex.ru>
2025-03-08 14:41:00 +0300

    + swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
+    app.setGlobalPrefix('api')
+    app.useGlobalPipes(new ValidationPipe())
+
+    app.enableCors({
+        origin: ['*'],
+        methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE'],
+        credentials: true
+    })
+
+    // Настройка Swagger
+    const config = new DocumentBuilder().setTitle('API Documentation').setDescription('Документация для API').setVersion('1.0').build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api/docs', app, document)
+
     await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
